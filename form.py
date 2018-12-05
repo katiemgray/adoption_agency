7bf476ebac6dec881556daf0d3857451a3ff60f5
KatieJessupMcD <katie.jessupmcd@gmail.com>
2018-12-04 16:02:21 -0800

    second commit

--- form.py
+++ form.py
@@ -2,29 +2,18 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, IntegerField, BooleanField
 from wtforms.validators import InputRequired, Optional, URL, AnyOf, NumberRange, Length
 
-
 class AddPetForm(FlaskForm):
     """form for adding pets"""
-    name = StringField(
-        "Pet Name", validators=[InputRequired(),
-                                Length(min=0, max=50)])
-    species = StringField(
-        "Species Name",
-        validators=[
-            InputRequired(),
-            AnyOf(values=['cat', 'dog', 'porcupine'])
-        ])
+    name = StringField("Pet Name", validators=[InputRequired(), Length(min=0, max=50)])
+    species = StringField("Species Name", validators=[InputRequired(), AnyOf(values=['cat', 'dog', 'porcupine'])])
     photo_url = StringField("Photo Url", validators=[Optional(), URL()])
-    age = IntegerField(
-        "Pet Age", validators=[InputRequired(),
-                               NumberRange(min=0, max=30)])
-    notes = TextAreaField(
-        "Pet Notes", validators=[Optional(),
-                                 Length(min=0, max=300)])
-
+    age = IntegerField("Pet Age", validators=[InputRequired(), NumberRange(min=0, max=30)])
+    notes = TextAreaField("Pet Notes", validators=[Optional(), Length(min=0, max=300)])
 
 class PetForm(FlaskForm):
     """form to display pet info and edit"""
     photo_url = StringField("Photo Url", validators=[Optional(), URL()])
-    notes = TextAreaField("Pet Notes", validators=[Optional()])
+    notes = TextAreaField("Pet Notes", validators=[Optional(), Length(min=0, max=300)])
     available = BooleanField("Available", validators=[Optional()])
+
+
