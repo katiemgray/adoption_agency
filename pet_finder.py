794602dcbedd93ac9e7b3890d2f15faf9d852b67
Nathan Chin <43153371+YozoraNoHoshi@users.noreply.github.com>
2018-12-04 16:18:06 -0800

    Some further study complete

--- pet_finder.py
+++ pet_finder.py
@@ -8,7 +8,10 @@ def random_pet():
 
     name = json_response['petfinder']['pet']['name']['$t']
     age = json_response['petfinder']['pet']['age']['$t']
-    photo_url = json_response['petfinder']['pet']['media']['photos']['photo'][
-        2]['$t']
+    try:
+        photo_url = json_response['petfinder']['pet']['media']['photos'][
+            'photo'][2]['$t']
+    except KeyError:
+        photo_url = 'https://vignette.wikia.nocookie.net/warriorcatsclanroleplay/images/f/fc/Placeholder-pet.png/revision/latest?cb=20130716185616'
 
     return {"name": name, "age": age, "photo_url": photo_url}
